4d8ec1764c4f96f0fb5946a7f10c29b1ae2c098c
B00144873 <B00144873@mytudublin.ie>
2022-10-11 22:09:40 +0100

    Added Unity project files and edited .gitignore file

---
+++ Challenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float bufferLength = 1.0f;
+    private float buffer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > buffer)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            buffer = Time.time + bufferLength; // Buffer is equal to the current time plus 1 second, meaning the player can send a dog again after the current time has passed the buffer
+        }
+    }
+}
